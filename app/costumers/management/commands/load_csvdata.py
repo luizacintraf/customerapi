ffe4153d68444163c320ece90ae52da7bd88bacc
Luiza Fernandes <luizacintraf@gmail.com>
2021-09-08 21:56:40 -0300

    create load csv management command

---
+++ app/costumers/management/commands/load_csvdata.py
@@ -0,0 +1,20 @@
+
+from django.core.management.base import BaseCommand
+from django.apps import apps
+import pandas
+
+class Command(BaseCommand):
+    help = 'Creating model objects according the file path specified'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--path', type=str, help="file path")
+        parser.add_argument('--model_name', type=str, help="model name")
+        parser.add_argument('--app_name', type=str, help="django app name that the model is connected to")
+
+    def handle(self, *args, **options):
+        #file_path = options['path']
+        #_model = get_model(options['app_name'], options['model_name'])
+        file_path='customers.csv'
+        _model = apps.get_model('costumers', 'Costumer')
+        _object_dict = pd.read_csv(file_path).to_dict(orient='records')
+        _object_dict.to_dict(orient='records')
