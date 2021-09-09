f7bc270f0bbcf316a4f6a0755fc9dfa3084c7ac7
Luiza Fernandes <luizacintraf@gmail.com>
2021-09-08 21:48:24 -0300

    create migrations

--- app/app/settings.py
+++ app/app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'costumers.apps.CostumersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
