92e0049e1c132063fe8e704b3530cd1bc59d50e5
Luiza Fernandes <luizacintraf@gmail.com>
2021-09-09 22:29:35 -0300

    create apis

--- app/costumers/urls.py
+++ app/costumers/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:id>/', views.get_costumer, name='costumer')
 ]
--- app/costumers/views.py
+++ app/costumers/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.core import serializers
+from costumers.models import Costumer
+from django.http.response import JsonResponse
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Test application")
+    return JsonResponse([ f['fields'] for f in serializers.serialize('python', Costumer.objects.all())], safe=False)
+def get_costumer(request,id):
+    if len(Costumer.objects.filter(id=id)) >0:
+        return JsonResponse(serializers.serialize('python', [Costumer(id=id)])[0]['fields'], safe=False, status=500)
+    else:
+        return JsonResponse({"error": "ID doesn't exists"})
