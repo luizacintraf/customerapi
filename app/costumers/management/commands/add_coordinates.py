d7d1f0ac40b4a5966b28fde5e7bf11abe61dffc6
Luiza Fernandes <luizacintraf@gmail.com>
2021-09-09 21:35:55 -0300

    add_coordinates function

--- app/costumers/management/commands/add_coordinates.py
+++ app/costumers/management/commands/add_coordinates.py
@@ -9,23 +9,24 @@ from costumers.models import Costumer
 class Command(BaseCommand):
     help = 'Add latitude and longitude'
 
-    def get_coordinates(self,address):
+    def get_coordinates(self, address):
+        import os
+        import requests
         api_key = os.environ['MAPS_API_KEY']
         try: 
             response = requests.get(f"https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={api_key}").json()
-            
-            return response['results'][0]['geometry']['location']
+            coordinates = response['results'][0]['geometry']['location']
         except:
-            address=address.split(',')[0]
+            address=str(address).split(',')[0]
             response = requests.get(f"https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={api_key}").json()
-            return response['results'][0]['geometry']['location']
-    def update_coordinates(self,costumer):
-        coordinates = self.get_coordinates(costumer.city)
-        costumer.latitude = coordinates['lat']
-        costumer.longitude = coordinates['lng']
-        costumer.save()
+            coordinates = response['results'][0]['geometry']['location']
+        return coordinates['lat'], coordinates['lng']
+
         
     def handle(self, *args, **options):
-        for costumer in Costumer.objects.all():
-            self.update_coordinates(costumer)
+        cities = set([costumer.city for costumer in Costumer.objects.all()])
+        for address in cities:
+            lat,lng = self.get_coordinates(address)
+            Costumer.objects.filter(city=address).update(
+            latitude=lat,longitude=lng)
 
